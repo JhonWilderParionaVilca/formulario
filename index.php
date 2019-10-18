13410771591762037d6851745bb07b4ca3c4a414
wilderPariona <jhon.pariona.27@unsch.edu.pe>
2019-10-17 20:06:12 -0500

    main

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FORMULARIO</title>
+
+    <!-- BOOSTRAP -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- css -->
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <header class="header">
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php">
+                <img src="img/logounsch.png" width="30" height="30" class="navbar-logo d-inline-block align-top" alt="">
+                <h1 class="navbar-title">Desarrollo Web-UNSCH</h1>
+            </a>
+        </nav>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Instructor: Mag. Ing. Hubner Janampa Patilla</h2>
+                </div>
+                <div class="col-12">
+                    <h3>Formulario Web</h3>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+    <main class="principal">
+        <div class="container border-radius">
+            <div class="row">
+                    
+            </div>
+        </div>
+
+    </main>
+
+
+
+
+
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
